85abcbb334361c37fdf549f684ffc640ba1d8bd5
Fayaz477 <116749059+Fayaz477@users.noreply.github.com>
2022-10-26 23:18:31 +0530

    Add files via upload

---
+++ excel8.py
@@ -0,0 +1,24 @@
+
+from openpyxl import Workbook,load_workbook
+from openpyxl.utils import get_column_letter
+import time
+wb = Workbook()
+sheet = wb.active
+
+
+sheet['A1'] = "StudentName"
+sheet['B1'] = "Mark"
+sheet['C1'] = "Status"
+limits=int(input("Enter the limits"))
+#for entering the values in excel
+for i in range(1,limits):
+    sheet["A"+str(i+1)]= input("Enter the student name:")
+    sheet["B"+str(i+1)]= input("Enter the mark:")
+    sheet["C"+str(i+1)]= input("Enter the status of student:")
+
+wb.save('Academic Details.xlsx')
+
+
+
+
+
