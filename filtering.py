1f269e047a078990d780cde04d15ed1ca7a4d76c
Fayaz477 <116749059+Fayaz477@users.noreply.github.com>
2022-10-26 23:30:28 +0530

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-df = pd.read_excel('Academic Details.xlsx') #reads the first sheet of your excel file
+df = pd.read_excel('Academic Details.xlsx',sheetname=0) #reads the first sheet of your excel file
+#for entering the filtering option
 choice=input("Enter the filtering choice fail / pass")
+#for checking the student status 
 if choice=="pass":
     df = df[(df['Status']=='pass')] #Filtering dataframe
     print(df)
